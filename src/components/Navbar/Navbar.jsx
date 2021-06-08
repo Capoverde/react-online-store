c6f1c70afc3704280bada57ba2b968ffeb45921d
Piotr <piotr_as@yahoo.com>
2021-06-08 14:02:11 +0200

    assets folder with logo icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import classes from '*.module.css';
+import logo from './assets/commerce.png';
+
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography variant="h6" classname={classes.title} color="inherit">
+                        <img src={} alt="Commerce.js" height="25px" className={classes.image} />
+                        Commerce.js
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </>
     )
 }
 
