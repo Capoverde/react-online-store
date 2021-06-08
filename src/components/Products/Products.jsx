1fff5a25a9cc01f11ec89fd9e0db05fc4a19ca31
Piotr <piotr.slobinski@gmail.com>
2021-06-08 17:36:37 +0200

    added margin to Navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
+import React from 'react'
+import { Grid } from '@material-ui/core'
 import Product from './Product/Product'
+import useStyles from './styles'
 
-const products =[
-    {id: 1, name: 'Shoes', description: "Running Shoes.", price:'$5' , image:"https://source.unsplash.com/200x200/?shoe"},
-    {id: 2, name: 'MacBook', description: "Apple macbook.", price:'$10', image:"https://source.unsplash.com/200x200/?laptop"},
+const products = [
+  { id: 1, name: 'Shoes', description: 'Running Shoes.', price: '$5', image: 'https://source.unsplash.com/200x200/?shoe' },
+  { id: 2, name: 'MacBook', description: 'Apple macbook.', price: '$10', image: 'https://source.unsplash.com/200x200/?laptop' }
 ]
 
 const Products = () => {
+  const classes = useStyles()
+
     return (
-        <main>
-            <Grid container justify="center" spacing={4} >
-                {products.map(( product ) => (
-                    <Grid item key={ product.id } xs={12} sm={6} md={4} lg={3}>
-                        <Product product={ product } />
-                   </Grid>
-                ))}
-            </Grid>
-        </main>
-    )
+    <main classname={classes.content}>
+    <div className={classes.toolbar}/>
+      <Grid container justify='center' spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  )
 }
 
-export default Products;
+export default Products
