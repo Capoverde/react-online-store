a5592ed8d899fab4f7d90151d9954abf28968539
Piotr <piotr_as@yahoo.com>
2021-06-09 12:05:45 +0200

    added Cart component + something is wrong with imports

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { Container, Typography,  Button, Grid } from '@material-ui/core';
 
-const Cart = () => {
+
+const Cart = ({ cart }) => {
+
+    const isEmpty = !cart.line_items.length;
+
+    const EmptyCart = () => (
+        <Typography variant='subtitle'>
+            You have no items in your shopping cart, start adding some
+        </Typography>
+    )
+
+    const filledCart = () =>(
+        <>
+            <Grid container spacing={3}>
+                { cart.line_items.map(() => (
+                    <Grid item xs={12} sm={4} key={item.id}>
+                        <CartItem />
+                    </Grid>
+                ))}
+
+            </Grid>
+        </>
+    )
+
+
+
     return (
-        <div>
-            
-        </div>
+        <Container>
+            <div classname={classes.tollbar} />
+            <Typography className={classes.title} variant="h3">
+                Your shopping card
+            </Typography>
+            { isEmpty ? <EmptyCart /> : <FilledCart /> }
+        </Container>
     )
 }
 
